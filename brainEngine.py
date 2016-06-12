f33f15440cf558473e59f82953fe6c6cd0a508cb
Rich <chich36@gmail.com>
2016-06-12 00:50:59 -0400

    Manual Train:Rouand 2
    You can now train the network manually for either jump or don't jump, however
    the player does must wait for the image stream to terminate before reacting.
    This means that it will never actually jump when it should.

--- brainEngine.py
+++ brainEngine.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import threading
 import numpy as np
 from array import array
@@ -20,6 +21,8 @@ class brainEngine (threading.Thread):
 
         self.image_old = np.zeros((self.scope_x,self.scope_z), dtype= int)
         self.image_sum = np.zeros((self.scope_x,self.scope_z), dtype= int)
+
+        self.stream_size = 0
         self.image_stream = False
         self.image_feed = False
         self.image_trig = False
@@ -34,20 +37,32 @@ class brainEngine (threading.Thread):
     def run(self):
         listqueue = [];
         while True:
-            image_new = np.array(self.sense.look(self.position), dtype=int)
-
-
+            buffer = np.array(self.sense.look(self.position), dtype=int)
 
             if (len(self.player.collision_history) > 0):
                 collision_data = self.player.collision_history.pop()
                 collision_time = collision_data[0]
                 other_player = collision_data[1]
                 print(' collision at: ', collision_time, 'between', self.playerID, ' and ', other_player)
-                self.image_stream = False
-                self.image_feed = True
+
+                print('Feeding Forward')
+                self.image_feed = False
+                # for row in self.image_sum.tolist():
+                #print('Image Sum\n', self.image_sum)
+                inputneurons = self.image_sum.reshape((1, self.scope_x * self.scope_z)) / self.stream_size;
+                #print('Input Neurons\n', inputneurons)
+                self.train_data = np.array(inputneurons)
+                followinstinct = self.myBrain.feedForwardOnly(inputneurons);
+                print('Instinct is: ', followinstinct)
+                self.stream_size = 0
                 self.lockout_flag = True
+                self.image_stream = False
+                self.image_feed = False
+
+
 
             if(not(self.lockout_flag)):
+                image_new = buffer
                 if (self.image_old.any() != image_new.any()):
                     ## Either begin or the end of a stream
                     if( self.image_old.any()):
@@ -61,6 +76,7 @@ class brainEngine (threading.Thread):
                         print('Starting Stream')
                 if(self.image_stream):
                     self.image_sum += image_new
+                    self.stream_size += 1
                     #print(image_new.any())
 
                 self.image_old = image_new
@@ -69,7 +85,8 @@ class brainEngine (threading.Thread):
                     self.image_feed = False
                     #for row in self.image_sum.tolist():
                     print(self.image_sum)
-                    inputneurons = self.image_sum.reshape((1,self.scope_x*self.scope_z));
+                    inputneurons = self.image_sum.reshape((1,self.scope_x*self.scope_z))/self.stream_size;
+                    #print(inputneurons)
                     self.train_data = np.array(inputneurons)
                     followinstinct = self.myBrain.feedForwardOnly(inputneurons);
                     print('Instinct is: ', followinstinct)
@@ -77,8 +94,6 @@ class brainEngine (threading.Thread):
                     if (followinstinct[0] == 'jump'):
                         self.player.chargeJump()
                         self.playerManager.jump(self.player)
-
-                    self.image_sum = np.zeros((self.scope_x,self.scope_z), dtype=int)
 
 
 
@@ -94,5 +109,13 @@ class brainEngine (threading.Thread):
         train_label = np_label.reshape((1,2))
         print(train_label)
 
+        #print('Image Sum\n', self.image_sum)
+        #print('Input Neurons\n', self.train_data)
+
         self.myBrain.trainAndSaveModel(self.train_data,train_label)
+
+        self.image_sum = np.zeros((self.scope_x, self.scope_z), dtype=int)
+        self.image_old = np.zeros((self.scope_x, self.scope_z), dtype=int)
+        self.stream_size = 0
         self.lockout_flag = False
+        self.image_feed = False
